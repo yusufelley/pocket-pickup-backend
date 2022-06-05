619868b86293bbf460f64dc8b176b45c63b6d8ac
Yusuf Elley <yelley2001@gmail.com>
2022-06-05 17:06:37 -0400

    add user db services

---
+++ utils/db-services/db.user-services.ts
@@ -0,0 +1,29 @@
+const addUser =
+  (User) =>
+  ({ fname, lname, email, profilePicURL }) => {
+    const user = new User({
+      fname,
+      lname,
+      email,
+      profilePicURL,
+    });
+    return user.save();
+  };
+
+const getAllUsers = (User) => () => {
+  return User.find({});
+};
+
+const getUserByEmail =
+  (User) =>
+  async ({ email }) => {
+    return await User.findOne({ email });
+  };
+
+module.exports = (User) => {
+  return {
+    addGoogleUser: addUser(User),
+    getUsers: getAllUsers(User),
+    getUserByEmail: getUserByEmail(User),
+  };
+};
