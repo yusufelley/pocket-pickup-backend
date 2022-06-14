e9cb3d3780bafeb17a6ba04d0d66c70af5ca868c
Yusuf Elley <yelley2001@gmail.com>
2022-06-13 23:50:49 -0400

    loadUser middleware

---
+++ middlewares/auth/loadUser.js
@@ -0,0 +1,61 @@
+import db from "../../utils/db-services/db.user-services";
+import { OAuth2Client } from "google-auth-library";
+
+const client = new OAuth2Client(process.env.CLIENT_ID);
+const verify = async (token) => {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID,
+  });
+  return ticket.getPayload();
+};
+
+export const loadUser = (req, res, next) => {
+  verify(req.body.token)
+    .then((payload) => {
+      console.log(`${payload.email} has been authenticated`);
+      db.getUserByEmail(payload.email)
+        .then(async (user) => {
+          if (user) {
+            // User already in DB
+            console.log("User found", user);
+            req.user = user;
+            console.log(req.user.fname);
+          } else {
+            // User not in DB
+            const newUser = {
+              fname: payload.given_name,
+              lname: payload.family_name,
+              email: payload.email,
+              profilePicURL: payload.picture,
+            };
+            console.log("User not found attempting to create ...", newUser);
+            await db.addUser(newUser);
+            console.log("New user added to database");
+            await db
+              .getUserByEmail(payload.email)
+              .then((user) => {
+                console.log("User found", user);
+                req.user = user;
+              })
+              .catch((err) => {
+                console.log("ERROR getting new user from DB", err);
+                res.send(
+                  "An error has occured the user could not be loaded from DB"
+                );
+              });
+          }
+          next();
+        })
+        .catch((err) => {
+          console.log("ERROR getting user in DB", err);
+          res.send("An error has occured the user could not be loaded from DB");
+        });
+    })
+    .catch((err) => {
+      console.log("ERROR validating JWT", err);
+      res.send("An error has occured the user could not be authenticated");
+    });
+};
+
+export default loadUser;
--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import { OAuth2Client } from "google-auth-library";
-import db from "./utils/db-services/db.user-services";
 import cors from "cors";
+import { UserAuthRequest } from "./custom";
+import loadUser from "./middlewares/auth/loadUser";
+
 // access environment variables
 dotenv.config();
 // create express app
@@ -21,78 +22,12 @@ mongoose.connect(process.env.DB_URI, {
 });
 
 // User Auth
-const client = new OAuth2Client(process.env.CLIENT_ID);
-export const verify = async (token: string) => {
-  const ticket = await client.verifyIdToken({
-    idToken: token,
-    audience: process.env.CLIENT_ID,
-  });
-  return ticket.getPayload();
-};
-
-// Load User
-app.use((req, res, next) => {
-  verify(req.body.token)
-    .then((payload) => {
-      console.log(`${payload.email} has been authenticated`);
-      db.getUserByEmail(payload.email)
-        .then(async (user) => {
-          if (user) {
-            // User already in DB
-            console.log("User found", user);
-            // req.user = user;
-          } else {
-            // User not in DB
-            const newUser = {
-              fname: payload.given_name,
-              lname: payload.family_name,
-              email: payload.email,
-              profilePicURL: payload.picture,
-            };
-            console.log("User not found attempting to create ...", newUser);
-            await db.addUser(newUser);
-            console.log("New user added to database");
-            db.getUserByEmail(payload.email)
-              .then((user) => console.log("User found", user))
-              .catch((err) =>
-                console.log("ERROR getting new user from DB", err)
-              );
-          }
-        })
-        .catch((err) => console.log("ERROR getting user in DB", err));
-      res.send(`Hello, ${payload.email}`);
-    })
-    .catch((err) => {
-      console.log("ERROR validating JWT", err);
-      res.send("An error has occured the user code not be authenticated");
-    });
-});
+app.use(loadUser);
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (req: UserAuthRequest, res) => {
+  res.send(`Hello, ${req.user.fname}`);
 });
-
-// app.post("/auth/google", (req, res) => {
-//   // Probably have to change query to body when React client is sending JWT
-//   verify(req.query.token)
-//     .then((payload) => {
-//       console.log(`${payload.email} has been authenticated`);
-//       db.getUserByEmail(payload.email).then((user) => {
-//         console.log(user);
-//       });
-//       // User does not exist
-//       if (db.getUserByEmail(payload.email)) {
-//       } else {
-//         // User exists
-//       }
-//       res.send(`Hello, ${payload.email}`);
-//     })
-//     .catch((err) => {
-//       console.error("ERROR:", err);
-//       res.send("An error has occured the user code not be authenticated");
-//     });
-// });
 
 app.listen(port, () => {
   console.log(`Pocket Pick-Up listening on port ${port}`);
