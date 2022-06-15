d87d79ae638db51efd8aebc2070ad4feef720156
Yusuf Elley <yelley2001@gmail.com>
2022-06-15 17:02:10 -0400

    add get user route

--- server.ts
+++ server.ts
@@ -13,20 +13,22 @@ const port = process.env.PORT || 5000;
 // parse incoming requests as JSON
 app.use(express.json());
 app.use(cors()); // Needed for dev
-
 // establish db connection
 mongoose.connect(process.env.DB_URI, {
   dbName: "pocket-pickup-db",
   user: "pocket-pickup-user",
   pass: process.env.DB_PASS,
 });
-
 // User Auth
 app.use(loadUser);
 
 // Routes
 app.get("/", (req: UserAuthRequest, res) => {
   res.send(`Hello, ${req.user.fname}`);
+});
+
+app.get("/user", (req: UserAuthRequest, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
