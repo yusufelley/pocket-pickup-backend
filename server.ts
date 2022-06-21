8b1c24113d0edd5adae0b7382aae6fa89c8e2ea8
Yusuf Elley <yelley2001@gmail.com>
2022-06-20 22:52:10 -0400

    https server setup

--- server.ts
+++ server.ts
@@ -4,11 +4,17 @@ import mongoose from "mongoose";
 import cors from "cors";
 import { UserAuthRequest } from "./custom";
 import loadUser from "./middlewares/auth/loadUser";
-
+import https from "https";
+import fs from "fs";
 // access environment variables
 dotenv.config();
+// https credentials
+const privateKey = fs.readFileSync("./key.pem", "utf8");
+const certificate = fs.readFileSync("./cert.pem", "utf8");
+const credentials = { key: privateKey, cert: certificate };
 // create express app
 const app = express();
+const httpsServer = https.createServer(credentials, app);
 const port = process.env.PORT || 5000;
 // parse incoming requests as JSON
 app.use(express.json());
@@ -31,6 +37,6 @@ app.get("/user", (req: UserAuthRequest, res) => {
   res.send(req.user);
 });
 
-app.listen(port, () => {
+httpsServer.listen(port, () => {
   console.log(`Pocket Pick-Up listening on port ${port}`);
 });
